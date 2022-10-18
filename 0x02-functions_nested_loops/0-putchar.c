11d91d886c70541c97b9fc1447f963483b82b155
Kagiso10 <thobakgale.kh@gmail.com>
2022-10-18 16:08:14 -0700

    k

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "main.h";
 /** main - prints putchar
  * Return: always 0
  */
@@ -12,6 +12,7 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
